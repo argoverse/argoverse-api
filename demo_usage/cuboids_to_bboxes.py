d8e0742c1b37166e2b2db087a63ddad6fbba32df
alliecc <mumumumoon@gmail.com>
2019-06-19 19:17:02 -0700

    fix centerline nan issue

--- demo_usage/cuboids_to_bboxes.py
+++ demo_usage/cuboids_to_bboxes.py
@@ -77,6 +77,10 @@ def plot_lane_centerlines_in_img(
 
         ground_heights = avm.get_ground_height_at_xy(centerline_city_fr, city_name)
 
+        valid_idx = np.isnan(ground_heights)
+        centerline_city_fr = centerline_city_fr[~valid_idx]
+
+
         centerline_egovehicle_fr = city_to_egovehicle_se3.inverse().transform_point_cloud(centerline_city_fr)
         centerline_uv_cam = cam_SE3_egovehicle.transform_point_cloud(centerline_egovehicle_fr)
 
