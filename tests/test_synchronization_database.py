e14a478b3c50d30400a28c2775833bb21057a54f
John Lambert <johnlamb@jrigoli-mobl.amr.corp.intel.com>
2019-06-22 13:45:16 -0700

    improve comments in test file

--- tests/test_synchronization_database.py
+++ tests/test_synchronization_database.py
@@ -6,8 +6,10 @@ import numpy as np
 from argoverse.data_loading.synchronization_database import find_closest_integer_in_ref_arr
 
 
-def test_find_closest_integer_in_ref_arr_middle():
+def test_find_closest_integer_in_ref_arr_middle() -> None:
     """
+    Verify that given a query integer, we can find the closest entry
+    in an integer array (if lands in middle of array).
 	"""
     query_int = 4
     ref_arr = np.array([0, 1, 2, 5, 6, 7], dtype=np.int64)
@@ -16,8 +18,10 @@ def test_find_closest_integer_in_ref_arr_middle():
     assert int_diff == 1
 
 
-def test_find_closest_integer_in_ref_arr_before():
+def test_find_closest_integer_in_ref_arr_before() -> None:
     """
+    Verify that given a query integer, we can find the closest entry
+    in an integer array (if lands before start of array).
 	"""
     query_int = 0
     ref_arr = np.array([2, 5, 6, 7], dtype=np.int64)
@@ -26,8 +30,10 @@ def test_find_closest_integer_in_ref_arr_before():
     assert int_diff == 2
 
 
-def test_find_closest_integer_in_ref_arr_after():
+def test_find_closest_integer_in_ref_arr_after() -> None:
     """
+    Verify that given a query integer, we can find the closest entry
+    in an integer array (if lands after the end of the array).
 	"""
     query_int = 9
     ref_arr = np.array([0, 1, 2, 5, 6, 7], dtype=np.int64)
