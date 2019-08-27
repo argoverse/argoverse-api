dae91100236b4f30aecf8a256c2f623a5c54bf31
janesjanes <patsorn.sangkloy@gmail.com>
2019-08-27 13:11:09 -0400

    update motp-i motp-o

--- argoverse/evaluation/eval_tracking.py
+++ argoverse/evaluation/eval_tracking.py
@@ -10,6 +10,8 @@ from typing import Any, Dict, List, TextIO, Union
 import motmetrics as mm
 import numpy as np
 
+from shapely.geometry.polygon import Polygon
+
 from argoverse.evaluation.eval_utils import get_pc_inside_bbox, label_to_bbox, leave_only_roi_region
 from argoverse.utils.json_utils import read_json_file
 from argoverse.utils.ply_loader import load_ply
@@ -35,10 +37,14 @@ def in_distance_range_pose(ego_center: np.ndarray, pose: np.ndarray, d_min: floa
         A boolean saying if input pose is with specified distance range.
     """
 
-    dist = float(np.linalg.norm(pose[0:2] - ego_center[0:2]))
+    dist = float(np.linalg.norm(pose[0:3] - ego_center[0:3]))
 
     return dist > d_min and dist < d_max
 
+def iou_polygon(poly1, poly2):
+    inter = poly1.intersection(poly2).area
+    union = poly1.union(poly2).area
+    return 1 - inter / union
 
 def get_distance(x1: np.ndarray, x2: np.ndarray, name: str) -> float:
     """Get the distance between two poses, returns nan if distance is larger than detection threshold.
@@ -51,10 +57,32 @@ def get_distance(x1: np.ndarray, x2: np.ndarray, name: str) -> float:
     Returns:
         A distance value or NaN
     """
+    if name == 'centroid':
+        dist = float(np.linalg.norm(x1[name][0:3] - x2[name][0:3]))
+        return dist if dist < 2.25 else float(np.nan)
+    elif name == 'iou':
+        w1 = x1['width']
+        l1 = x1['length']
 
-    dist = float(np.linalg.norm(x1[name][0:2] - x2[name][0:2]))
-    return dist if dist < 2 else float(np.nan)
+        w2 = x2['width']
+        l2 = x2['length']
 
+        poly1 = Polygon([(-l1/2, -w1/2),(-l1/2, w1/2), (l1/2, w1/2) , (l1/2, -w1/2)])
+        poly2 = Polygon([(-l2/2, -w2/2),(-l2/2, w2/2), (l2/2, w1/2) , (l2/2, -w2/2)])
+
+        inter = poly1.intersection(poly2).area
+        union = poly1.union(poly2).area
+        return 1 - inter / union
+
+    elif name == 'orientation':
+        return np.abs(x1[name]-x2[name])*180/np.pi
+    else:
+        raise ValueError('Not implemented..')
+
+def get_forth_vertex_rect(p1, p2, p3):
+    x = p2[0] - p1[0] + p3[0]
+    y = p3[1] - p1[1] + p2[1]
+    return [x, y]
 
 def eval_tracks(
     path_tracker_output: str,
@@ -74,7 +102,10 @@ def eval_tracks(
         out_file: output file object
         centroid_method: method for ground truth centroid estimation
     """
-    acc = mm.MOTAccumulator(auto_id=True)
+    acc_c = mm.MOTAccumulator(auto_id=True)
+    acc_i = mm.MOTAccumulator(auto_id=True)
+    acc_o = mm.MOTAccumulator(auto_id=True)
+
 
     path_track_data = sorted(glob.glob(os.fspath(path_tracker_output) + "/*"))
 
@@ -127,6 +158,9 @@ def eval_tracks(
 
                 gt[track_label_uuid]["bbox"] = bbox
                 gt[track_label_uuid]["orientation"] = orientation
+                gt[track_label_uuid]["width"] = gt_data[i]["width"]
+                gt[track_label_uuid]["length"] = gt_data[i]["length"]
+                gt[track_label_uuid]["height"] = gt_data[i]["height"]
 
                 if track_label_uuid not in ID_gt_all:
                     ID_gt_all.append(track_label_uuid)
@@ -145,25 +179,39 @@ def eval_tracks(
                 continue
 
             center = np.array([track["center"]["x"], track["center"]["y"], track["center"]["z"]])
-
+            bbox, orientation = label_to_bbox(track)
             if in_distance_range_pose(np.zeros(3), center, d_min, d_max):
                 tracks[key] = {}
                 tracks[key]["centroid"] = center
+                tracks[key]["bbox"] = bbox
+                tracks[key]["orientation"] = orientation
+                tracks[key]["width"] = track["width"]
+                tracks[key]["length"] = track["length"]
+                tracks[key]["height"] = track["height"]
 
                 id_tracks.append(key)
 
-        dists: List[List[float]] = []
+        dists_c: List[List[float]] = []
+        dists_i: List[List[float]] = []
+        dists_o: List[List[float]] = []
         for gt_key, gt_value in gt.items():
-            gt_track_data: List[float] = []
-            dists.append(gt_track_data)
+            gt_track_data_c: List[float] = []
+            gt_track_data_i: List[float] = []
+            gt_track_data_o: List[float] = []
+            dists_c.append(gt_track_data_c)
+            dists_i.append(gt_track_data_i)
+            dists_o.append(gt_track_data_o)
             for track_key, track_value in tracks.items():
-                gt_track_data.append(get_distance(gt_value, track_value, "centroid"))
+                gt_track_data_c.append(get_distance(gt_value, track_value, "centroid"))
+                gt_track_data_i.append(get_distance(gt_value, track_value, "iou"))
+                gt_track_data_o.append(get_distance(gt_value, track_value, "orientation"))
 
-        acc.update(id_gts, id_tracks, dists)
+        acc_c.update(id_gts, id_tracks, dists_c)
+        acc_i.update(id_gts, id_tracks, dists_i)
+        acc_o.update(id_gts, id_tracks, dists_o)
 
-    mh = mm.metrics.create()
     summary = mh.compute(
-        acc,
+        acc_c,
         metrics=[
             "num_frames",
             "mota",
@@ -184,7 +232,7 @@ def eval_tracks(
     fn = os.path.basename(path_tracker_output)
     num_frames = summary["num_frames"][0]
     mota = summary["mota"][0] * 100
-    motp = summary["motp"][0]
+    motp_c = summary["motp"][0]
     idf1 = summary["idf1"][0]
     most_track = summary["mostly_tracked"][0] / num_tracks
     most_lost = summary["mostly_lost"][0] / num_tracks
@@ -193,8 +241,37 @@ def eval_tracks(
     num_switch = summary["num_switches"][0]
     num_flag = summary["num_fragmentations"][0]
 
+    acc_c.events.loc[acc_c.events.Type != 'RAW','D'] = acc_i.events.loc[acc_c.events.Type != 'RAW','D']
+
+    sum_motp_i = mh.compute(
+        acc_c,
+        metrics=[
+            "motp",
+        ],
+        name="acc",
+    )
+    logger.info("MOTP-I = %s", sum_motp_i)
+    num_tracks = len(ID_gt_all)
+
+    fn = os.path.basename(path_tracker_output)
+    motp_i = sum_motp_i["motp"][0]
+
+    acc_c.events.loc[acc_c.events.Type != 'RAW','D'] = acc_o.events.loc[acc_c.events.Type != 'RAW','D']
+    sum_motp_o = mh.compute(
+        acc_c,
+        metrics=[
+            "motp",
+        ],
+        name="acc",
+    )
+    logger.info("MOTP-O = %s", sum_motp_o)
+    num_tracks = len(ID_gt_all)
+
+    fn = os.path.basename(path_tracker_output)
+    motp_o = sum_motp_o["motp"][0]
+
     out_string = (
-        f"{fn} {num_frames} {mota:.2f} {motp:.2f} {idf1:.2f} {most_track:.2f} "
+        f"{fn} {num_frames} {mota:.2f} {motp_c:.2f} {motp_o:.2f} {motp_i:.2f} {idf1:.2f} {most_track:.2f} "
         f"{most_lost:.2f} {num_fp} {num_miss} {num_switch} {num_flag} \n"
     )
     out_file.write(out_string)
