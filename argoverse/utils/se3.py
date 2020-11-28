388e3832e6b71dd07f6e64955f8a015dffc31768
John Lambert <johnlambert@gatech.edu>
2020-11-28 00:01:40 -0500

    add more clear compose function that is alias for right_multiply_with_se3

--- argoverse/utils/se3.py
+++ argoverse/utils/se3.py
@@ -55,8 +55,8 @@ class SE3:
         """
         return SE3(rotation=self.rotation.T, translation=self.rotation.T.dot(-self.translation))
 
-    def right_multiply_with_se3(self, right_se3: "SE3") -> "SE3":
-        """Right multiply the transformation matrix with another SE3 instance.
+    def compose(self, right_se3: "SE3") -> "SE3":
+        """Compose (right multiply) this class' transformation matrix T with another SE3 instance.
 
         Algebraic representation: chained_se3 = T * right_se3
 
@@ -72,3 +72,17 @@ class SE3:
             translation=chained_transform_matrix[:3, 3],
         )
         return chained_se3
+
+    def right_multiply_with_se3(self, right_se3: "SE3") -> "SE3":
+        """Compose (right multiply) this class' transformation matrix T with another SE3 instance.
+
+        Algebraic representation: chained_se3 = T * right_se3
+
+        Args:
+            right_se3: instance of SE3 class
+
+        Returns:
+            chained_se3: instance of SE3 class
+        """
+        return self.compose(right_se3)
+    
