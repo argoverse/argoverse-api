a32975b19fae70e6f5d167524d6f8a6d9cb4365b
John Lambert <jlambert@argo.ai>
2021-05-07 00:05:17 -0400

    put constants in a global config

---
+++ argoverse/sensor_dataset_config.py
@@ -0,0 +1,35 @@
+
+from typing import NamedTuple, Optional, Tuple
+
+from hydra.experimental import compose, initialize_config_module
+from hydra.utils import instantiate
+
+
+class ImageDimensions(NamedTuple):
+    """Dimensions are provided as (height, width)"""
+    height: int
+    width: int
+
+class SensorDimensions(NamedTuple):
+    ring_front_center: ImageDimensions
+    ring_front_left: ImageDimensions
+    ring_front_right: ImageDimensions
+    ring_side_left: ImageDimensions
+    ring_side_right: ImageDimensions
+    ring_rear_left: ImageDimensions
+    ring_rear_right: ImageDimensions
+    stereo_front_right: Optional[ImageDimensions]
+    stereo_front_left: Optional[ImageDimensions]
+
+class SensorDatasetConfig(NamedTuple):
+    dataset_name: str
+    ring_cam_fps: int
+    stereo_cam_fps: int
+    sensors: SensorDimensions
+
+DATASET_NAME = "argoverse1.1"
+
+with initialize_config_module(config_module="argoverse.config"):
+    cfg = compose(config_name="argoverse1.1.yml")
+    ArgoverseConfig: SensorDatasetConfig = instantiate(cfg.SensorDatasetConfig)
+
