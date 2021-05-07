c237c0732aed85393a7ba1632b54e3976eb63108
John Lambert <jlambert@argo.ai>
2021-05-07 00:31:56 -0400

    improve docstrings

--- argoverse/sensor_dataset_config.py
+++ argoverse/sensor_dataset_config.py
@@ -1,4 +1,3 @@
-
 from typing import NamedTuple, Optional
 
 from hydra.experimental import compose, initialize_config_module
@@ -6,11 +5,15 @@ from hydra.utils import instantiate
 
 
 class ImageDimensions(NamedTuple):
-    """Dimensions are provided as (height, width)"""
+    """Dimensions are provided in pixels."""
+
     height: int
     width: int
 
+
 class SensorDimensions(NamedTuple):
+    """Contains information about image dimensions for each sensor."""
+
     ring_front_center: ImageDimensions
     ring_front_left: ImageDimensions
     ring_front_right: ImageDimensions
@@ -21,15 +24,18 @@ class SensorDimensions(NamedTuple):
     stereo_front_right: Optional[ImageDimensions]
     stereo_front_left: Optional[ImageDimensions]
 
+
 class SensorDatasetConfig(NamedTuple):
+    """Global constants regarding frame rate and image dimensions."""
+
     dataset_name: str
     ring_cam_fps: int
     stereo_cam_fps: int
     sensors: SensorDimensions
+
 
 DATASET_NAME = "argoverse1.1"
 
 with initialize_config_module(config_module="argoverse.config"):
     cfg = compose(config_name=f"{DATASET_NAME}.yml")
     ArgoverseConfig: SensorDatasetConfig = instantiate(cfg.SensorDatasetConfig)
-
