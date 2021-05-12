b76d0082dcf89fdfc977b47cb472764cc697a0b2
John Lambert <jlambert@argo.ai>
2021-05-12 15:10:06 -0400

    add missing docstrings for to_metric_time()

--- argoverse/utils/metric_time.py
+++ argoverse/utils/metric_time.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+from typing import Union
 
 import numpy as np
 
@@ -10,8 +11,17 @@ class TimeUnit(Enum):
     Nanosecond = auto()
 
 
-def to_metric_time(ts: int, src: TimeUnit, dst: TimeUnit) -> float:
-    """ """
+def to_metric_time(ts: Union[int, float], src: TimeUnit, dst: TimeUnit) -> float:
+    """Convert a timestamp from src units of metric time, to dst units.
+
+    Args:
+        ts: timestamp, expressed either as an integer or float, measured in `src` units of metric time
+        src: source unit of metric time
+        dst: destination/target unit of metric time
+
+    Returns:
+        timestamp expressed now in `dst` units of metric time
+    """
     units_per_sec = {TimeUnit.Second: 1, TimeUnit.Millisecond: 1e3, TimeUnit.Microsecond: 1e6, TimeUnit.Nanosecond: 1e9}
     # ts is in units of `src`, which will cancel with the denominator
     return ts * (units_per_sec[dst] / units_per_sec[src])
