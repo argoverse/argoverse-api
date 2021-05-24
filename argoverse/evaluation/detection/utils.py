44f59721ee5e3a32e5639c8a857d683df078fbce
Benjamin Wilson <benjaminrwilson@noreply.github.com>
2021-05-24 15:37:30 +0000

    Revert roi flag.

--- argoverse/evaluation/detection/utils.py
+++ argoverse/evaluation/detection/utils.py
@@ -95,7 +95,7 @@ class DetectionCfg(NamedTuple):
     save_figs: bool = True
     tp_normalization_terms: np.ndarray = np.array([tp_thresh, MAX_SCALE_ERROR, MAX_YAW_ERROR])
     summary_default_vals: np.ndarray = np.array([MIN_AP, tp_thresh, MAX_NORMALIZED_ASE, MAX_NORMALIZED_AOE, MIN_CDS])
-    eval_only_roi_instances: bool = False
+    eval_only_roi_instances: bool = True
     map_root: _PathLike = Path(__file__).parent.parent.parent.parent / "map_files"  # argoverse-api/map_files
 
 
