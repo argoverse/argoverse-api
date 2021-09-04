528d4e3c7f75fbe850fda1035c2f120d80365614
Benjamin Wilson <bwilson@argo.ai>
2021-09-04 23:13:00 +0000

    Update io stubs.

--- src/argoverse/io/loading.py
+++ src/argoverse/io/loading.py
@@ -1,18 +1,25 @@
+"""I/O for manipulating the Argoverse 2.0 dataset."""
+from pathlib import Path
+from typing import Optional, Tuple
 import numpy as np
 import pandas as pd
 
 
-def load_calibration() -> pd.DataFrame:
-    pass
+def load_calibration(fpath: Path, columns: Optional[Tuple[str, ...]]) -> pd.DataFrame:
+    calibration = pd.read_feather(fpath)
+    return calibration
 
 
-def load_labels() -> pd.DataFrame:
-    pass
+def load_labels(fpath: Path, columns: Optional[Tuple[str, ...]]) -> pd.DataFrame:
+    labels = pd.read_feather(fpath)
+    return labels
 
 
-def load_lidar() -> pd.DataFrame:
-    pass
+def load_lidar(fpath: Path, columns: Optional[Tuple[str, ...]]) -> pd.DataFrame:
+    lidar = pd.read_feather(fpath)
+    return lidar
 
 
-def load_poses() -> pd.DataFrame:
-    pass
+def load_poses(fpath: Path, columns: Optional[Tuple[str, ...]]) -> pd.DataFrame:
+    poses = pd.read_feather(fpath)
+    return poses
