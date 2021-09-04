7fc6acfc33197cfde810bf2d4ad02f10709e6034
Benjamin Wilson <bwilson@argo.ai>
2021-09-04 23:30:49 +0000

    Add io testing stubs.

---
+++ tests/io/test_loading.py
@@ -0,0 +1,17 @@
+"""Unit testing for loading the Argoverse 2.0 sensor dataset schema."""
+
+
+def test_load_calibration() -> None:
+    pass
+
+
+def test_load_labels() -> None:
+    pass
+
+
+def test_load_lidar() -> None:
+    pass
+
+
+def test_load_poses() -> None:
+    pass
