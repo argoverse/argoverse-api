8add7711c6e37c2585f684d8435552752d62340a
Benjamin Wilson <bwilson@argo.ai>
2021-09-04 23:04:17 +0000

    Add loading stubs.

---
+++ src/argoverse/io/loading.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+
+def load_calibration() -> pd.DataFrame:
+    pass
+
+
+def load_labels() -> pd.DataFrame:
+    pass
+
+
+def load_lidar() -> pd.DataFrame:
+    pass
+
+
+def load_poses() -> pd.DataFrame:
+    pass
