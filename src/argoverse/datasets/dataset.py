3b008d1b38eed8e8ed7c0e7f24b51b08b16e1f94
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2021-09-22 10:12:30 -0400

    Cleanup dataset.py

--- src/argoverse/datasets/dataset.py
+++ src/argoverse/datasets/dataset.py
@@ -1,16 +1,10 @@
-import os
 import os.path as osp
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Dict, List, Optional, Tuple
-from tqdm import tqdm
+from typing import Optional, Tuple
 
-import pandas as pd
 import polars as pl
 from polars.eager import DataFrame
-from polars.lazy import count
-
-from focal.datasets.argoverse_utils import AV1_MAPPING
 
 
 @dataclass
