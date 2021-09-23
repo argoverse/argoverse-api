69a346642acc4b5dff4d7c9e46b9dbe612edd754
Benjamin Wilson <benjaminrwilson@noreply.github.com>
2021-09-23 16:10:33 +0000

    Add dataset.

--- src/argoverse/datasets/dataset.py
+++ src/argoverse/datasets/dataset.py
@@ -1,10 +1,15 @@
+import logging
 import os.path as osp
+import sys
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Tuple
 
-import polars as pl
 from polars.eager import DataFrame
+from polars.io import read_ipc
+
+logger = logging.Logger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 @dataclass
@@ -12,12 +17,11 @@ class Dataset:
 
     rootdir: Path
     index_names: Tuple[str, ...] = field(default_factory=tuple)
-    metadata: Optional[DataFrame] = None
+    metadata: DataFrame = field(init=False)
 
     def __post_init__(self):
         self.crawl()
 
     def crawl(self):
         metadata_file = osp.join(self.rootdir, "_metadata")
-        self.metadata = pl.io.read_ipc(metadata_file, use_pyarrow=False)
-
+        self.metadata = read_ipc(metadata_file, use_pyarrow=False)
