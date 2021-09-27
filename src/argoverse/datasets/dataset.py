71fffd673a1d3a22a652b1dcd4fedc0bf8fd7325
Benjamin Wilson <benjaminrwilson@noreply.github.com>
2021-09-27 20:23:17 +0000

    Add docstrings.

--- src/argoverse/datasets/dataset.py
+++ src/argoverse/datasets/dataset.py
@@ -1,3 +1,4 @@
+"""General dataloader for Argoverse 2.0 datasets."""
 import logging
 import os.path as osp
 from dataclasses import dataclass, field
@@ -17,14 +18,20 @@ logger = logging.Logger(__name__)
 @dataclass
 class Dataset:
 
-    rootdir: Path
+    rootdir: Path 
     index_names: Tuple[str, ...]
     metadata: DataFrame = field(init=False)
 
     def __post_init__(self):
+        """Post initialization."""
         self.crawl()
 
     def crawl(self):
+        """Crawl the metadata files.
+
+        Load the root metadata file for fasting dataloading / indexing.
+        This prevents overloading the filesystem with I/O operations.
+        """
         metadata_file = osp.join(self.rootdir, "_metadata")
         self.metadata = read_ipc(metadata_file, use_pyarrow=False)
 
@@ -37,6 +44,19 @@ class Dataset:
         return [Path(self.rootdir, key, "part.feather") for key in keys]
 
     def load_data(self, metadata: DataFrame) -> List[DataFrame]:
+        """Load the data from the provided metadata.
+
+        Constructs the absolute paths of the dataset from from the
+        root directory and the keys in the metadata file. Parallelizes
+        dataloading using all cores on the machine with a chunksize
+        of `metadata.shape[0] / num_cpus`.
+
+        Args:
+            metadata (DataFrame): (N,34) DataFrame.
+
+        Returns:
+            List[DataFrame]: (N,) Dataframes of variable size.
+        """
         paths = self.get_paths(metadata)
         chunksize = compute_chunksize(len(paths))
         paths = [(path, self.index_names) for path in paths]
