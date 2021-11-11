c3ca68eaa80b424b647b3674a7d96b4364a36b19
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2021-11-11 03:43:43 +0000

    Add pinhole model.

--- src/argoverse/io/loading.py
+++ src/argoverse/io/loading.py
@@ -1,23 +1,11 @@
-"""I/O for manipulating the Argoverse 2.0 dataset."""
+"""I/O for manipulating AV2."""
+
 from pathlib import Path
-from typing import Tuple
+from typing import Optional, Tuple
 
 import pandas as pd
-from polars import from_pandas
-from polars.eager import DataFrame
-from polars.io import read_ipc
+from pyarrow import feather
 
 
-def read_feather(path: Path, index_names: Tuple[str, ...]) -> DataFrame:
-    if "lidar" in str(path):
-        return read_lidar(path)
-
-    data = read_ipc(path, use_pyarrow=False)
-    for i, column_name in enumerate(index_names[::-1]):
-        parent = path.parents[i]
-        data[column_name] = [parent.stem for _ in range(len(data))]
-    return data
-
-
-def read_lidar(path: Path) -> DataFrame:
-    return from_pandas(pd.read_feather(path).astype(float))
+def read_feather(path: Path, columns: Optional[Tuple[str, ...]] = None) -> pd.DataFrame:
+    return feather.read_feather(path, columns=columns)
