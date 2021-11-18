825839a09ed107b5a0c07bf6022808a5d7444cdb
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2021-11-18 20:48:16 +0000

    Update typing.

--- src/argoverse/utils/typing.py
+++ src/argoverse/utils/typing.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
 import os
 import typing
+from typing import TYPE_CHECKING
 
 PathLike = typing.Union[str, bytes, os.PathLike[str]]
+
+if TYPE_CHECKING:
+    reveal_locals()
